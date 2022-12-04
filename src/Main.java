02112cb87cff30ad90fcefa86e2d6d976724747e
Christian Felipe Carvalho <64433548+christianfelipecarvalho@users.noreply.github.com>
2022-12-04 15:56:06 -0300

    Criando menu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("ola");
         System.out.println("hello");
     }
+    private static void menuOpcao(){
+    }
 }
+
